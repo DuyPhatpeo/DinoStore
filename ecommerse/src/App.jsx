95f2c428be32782112644e6c132a31b626deaf9d
Tran Duy Phat <phattranduy00@gmail.com>
2025-07-08 21:27:57 +0700

    Tạo Folder Structure SCSS

--- ecommerse/src/App.jsx
+++ ecommerse/src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import Hello from "@components/Hello/Hello";
+import Button from "@components/Button/Button";
+import MainLayout from "./components/Layout/Layout";
+import MyHeader from "./components/Header/Header";
+import MyFooter from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
-      <div>Ecommerse</div>
+      <MainLayout>
+        <MyHeader />
+        Content
+        <MyFooter />
+      </MainLayout>
     </>
   );
 }
