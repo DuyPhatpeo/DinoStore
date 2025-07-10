fbd39e87b3e6dd1acba779c621da465fab0a323a
Tran Duy Phat <phattranduy00@gmail.com>
2025-07-10 17:49:41 +0700

    thay đổi logo

--- ecommerse/src/components/Header/Logo/Logo.jsx
+++ ecommerse/src/components/Header/Logo/Logo.jsx
@@ -1,3 +1,16 @@
+import logo from "@images/Logo.png";
+
 export default function Logo() {
-  return <h1 className="text-2xl font-bold sm:text-3xl">Dino Store</h1>;
+  return (
+    <div className="flex items-center space-x-2">
+      <img
+        src={logo}
+        alt="Dino Store Logo"
+        className="h-10 w-10 sm:h-10 sm:w-10 object-contain"
+      />
+      <span className="text-xl font-semibold text-gray-800 sm:text-2xl">
+        Dino Store
+      </span>
+    </div>
+  );
 }
