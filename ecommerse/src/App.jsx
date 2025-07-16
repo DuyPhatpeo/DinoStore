ef5a0fc862fc105dfb1c82ac82ea90377254aeae
Tran Duy Phat <phattranduy00@gmail.com>
2025-07-16 09:00:35 +0700

    fix bug

--- ecommerse/src/App.jsx
+++ ecommerse/src/App.jsx
@@ -34,7 +34,6 @@ function App() {
                 />
               ))}
             </Routes>
-            <ToastTester />
             <ScrollToTopButton />
           </Suspense>
         </BrowserRouter>
